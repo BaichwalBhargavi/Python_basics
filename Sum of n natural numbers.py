2067df8a28c49e5d0bf6be87029dcd9e2e0a593b
BaichwalBhargavi <bhargavibaichwal29@gmail.com>
2025-05-01 16:09:37 +0530

    Create Sum of n natural numbers.py

---
+++ Sum of n natural numbers.py
@@ -0,0 +1,15 @@
+# Sum of first n natural numbers (using for loop)
+n= 5
+a=0
+for i in range (n+1):
+    a=a+i
+
+print(a)
+
+# Sum of first n natural numbers (using recursion)
+def sum(n):
+    if n == 0:
+        return 0
+    return n + sum(n-1)
+
+print(sum(5))
